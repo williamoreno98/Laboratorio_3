1143aa36bfe2462a07ac66974297c6bce20f56f0
williamoreno98 <61461094+williamoreno98@users.noreply.github.com>
2020-04-25 10:30:25 -0500

    Add files via upload

--- estados.ino
+++ estados.ino
@@ -1,4 +1,4 @@
-int incomingByte = 0; // for incoming serial data
+
 #define ESTADO0 0
 #define ESTADO1 1
 #define ESTADO2 2
@@ -10,32 +10,106 @@ int incomingByte = 0; // for incoming serial data
 #define ESTADO8 8
 #define ESTADO9 9
 #define ESTADO6 10
+#define PALABRAINICIO '#'
+#define OPCION1 '1'
+#define OPCION2 '2'
+#define OPCION3 '3'
 
-int estado=ESTADO0;
+int estado=0;
+char incomingByte;
+char dato[2]={0,0};
+int cont=0;
+
+uint8_t XORChecksum8(const byte *dato , int largo){
+     uint8_t valor=0;
+
+     for(int i=0; i< largo ; i++){
+        valor^=(uint8_t)(*dato);
+        dato++;
+      }
+      return ~valor;
+}
+
 void setup() {
   Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
 }
 int n=0;
 void loop() {
   // send data only when you receive data:
+
+  if(Serial.available()>0){
+         incomingByte = Serial.read();
+         if(n=0){
+          estado= ESTADO0;
+         }
   
   switch(estado){
 
     case ESTADO0:
-         
-         Serial.write(4);
-         
-        //if(Serial.available()>0){
-        
-          incomingByte = Serial.read();
-         Serial.print("I received: ");
-         Serial.println(incomingByte, DEC);
-         estado= ESTADO1;
-        //}
-       estado= ESTADO0; 
+
+          if(incomingByte== PALABRAINICIO){
+              Serial.println("Llego bien");
+              estado= ESTADO1;
+              n=1;
+            }
+          else {
+              estado= ESTADO0;
+            }
        break;
+       
     case ESTADO1:
-       break;
-     }
+
+        if(incomingByte== OPCION1 ){
+            Serial.println("Elegiste opcion 1 para escribir");
+            estado= ESTADO2;
+            cont=0;
+            break;
+          }
+        if(incomingByte== OPCION2 ){
+            Serial.println("Elegiste opcion 2 para leer todo");    
+            estado= ESTADO3;
+            break;
+        }
+        if(incomingByte == OPCION3){
+            Serial.println("Elegiste opcion 3 para leer un dato");  
+            estado= ESTADO4;
+          break;
+        }
+        else{
+          estado= ESTADO1;
+          break;
+          } 
+    
+
+    case ESTADO2: 
+        dato[cont]=incomingByte;
+        cont++;
+        if(cont==2){
+          estado= ESTADO5;
+        }
+    
+      break;
+    case ESTADO5: 
+
+      Serial.println(dato[0]);
+      Serial.println(dato[1]);
+
+      if(incomingByte==XORChecksum8(&dato[0],2)){
+        cont=0;
+        Serial.println("CRC bien");
+        }
+      else{
+        Serial.println("CRC mal");
+                }
+
+
+
+    break;
+  }
+
+
+       
+     
+
+  }
 }
-  
