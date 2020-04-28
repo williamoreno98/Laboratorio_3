18d3ceb9ac96c7dc2ae413fdcd092fbfdee6b247
williamoreno98 <61461094+williamoreno98@users.noreply.github.com>
2020-04-28 17:09:59 -0500

    Add files via upload

---
+++ probar.ino
@@ -0,0 +1,47 @@
+#include "Wire.h"
+#define EEPROM_I2C_ADDRESS 0x50
+
+int address=0;
+long int value=0;
+
+
+void setup() {
+
+  Wire.begin();
+  Serial.begin(9600);
+
+  //write process 
+//
+//  for(int i=0; i<256 ;i++){
+//  value=i;
+//  address= address+1;
+//  Wire.beginTransmission(EEPROM_I2C_ADDRESS);  // make control byte to begin the transmission
+//  Wire.write((int)(address >> 8));  // high address byte
+//  Wire.write((int)(address & 0xFF)); // low address byte
+//  Wire.write(value);
+//  Wire.endTransmission();
+//  delay(5);
+//  }
+  // read process
+
+  for(int i=0; i<256 ;i++){
+  long int readata=0;
+  Wire.beginTransmission(EEPROM_I2C_ADDRESS);  // make control byte to begin the transmission
+  Wire.write((int)(address >> 8));  // high address byte
+  Wire.write((int)(address & 0xFF)); // low address byte
+  Wire.endTransmission();
+  Wire.requestFrom(EEPROM_I2C_ADDRESS,1); // pedir a la memoria leer
+  readata= Wire.read(); //leer dato y guardar
+
+  Serial.print(address);
+  Serial.print(" : ");
+  Serial.println(readata);
+    address= address+1;
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  
+}
